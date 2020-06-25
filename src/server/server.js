bca27b6a70953574cd27e7a7e7d987f37151ee38
isaac <aderonmuisaac@gmail.com>
2020-06-25 22:09:56 +0100

    feat: setup express server

---
+++ src/server/server.js
@@ -0,0 +1,31 @@
+  
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+const bodyParser = require('body-parser')
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log(server);
+    console.log(`running on localhost: ${port}`);
+};
