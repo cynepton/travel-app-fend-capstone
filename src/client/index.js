2297b2ad1d358ae66a5eeabcde5508c81f92ad6d
isaac <aderonmuisaac@gmail.com>
2020-06-26 15:46:56 +0100

    feat: import geoNamesAPI

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,12 @@
+// Imported JavaScript functions
+import {geoNamesAPI} from './js/geonamesAPI'
+
+// Imported SASS styles
 import './styles/sass/main.scss';
 import './styles/sass/trip-card.scss';
+
+window.addEventListener('DOMContentLoaded', geoNamesAPI);
+
+export {
+    geoNamesAPI
+}
