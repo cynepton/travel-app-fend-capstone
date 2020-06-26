1c190636de7f2ae4ac0ab323a7f95eee3138b648
isaac <aderonmuisaac@gmail.com>
2020-06-26 01:17:39 +0100

    chore: set express folder to dist

--- src/server/server.js
+++ src/server/server.js
@@ -19,7 +19,7 @@ const cors = require('cors');
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('dist'));
 
 // Setup Server
 const port = 3000;
